3814a342f9c81f3cc5c58d206cfbcf293037b50b
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-04-30 01:03:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,38 +23,29 @@
 //   callPromise().then(alert).catch(alert);
 // }
 
+                                                                                                                                                                                                                                                          //your JS code here. If required.
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
 
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput.value) >= 18) {
+        resolve({ name: nameInput.value, age: Number(ageInput.value) });
+      } else {
+        reject({ name: nameInput.value });
+      }
+    }, 4000);
+  });
 
-
-//your JS code here. If required.
-var nameElement = document.querySelector("#name");
-var ageElement = document.querySelector("#age");
-var btnElement = document.querySelector("#btn");
-
-var promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    var age = ageElement.value;
-    var name = nameElement.value;
-   
-   if (!(age && name)) {
-     alert("Please enter valid details");
-    return;
-   }
-    if (age < 18) {
-      reject(`Oh sorry ${name}. You aren't old enough`);
-    } else {
-      resolve(`Welcome, ${name}. You can vote.`);
-    }
-  }, 4000);
+  promise.then((data) => {
+      alert(`Welcome, ${data.name}. You can vote.`);
+    })
+    .catch((error) => {
+      alert(`Oh sorry ${error.name}. You aren't old enough.`);
+    });
 });
-
-function callPromise() {
-    return promise;
-}
-function handleSubmit(e) {
-    e.preventDefault();
-  callPromise().then(alert).catch(alert);
-}
-btnElement.addEventListener("click", handleSubmit);
 
 btnElement.addEventListener("click", handleSubmit);
